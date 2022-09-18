e7598b82da1944c7784119445b3fa8834ccd2c76
swaraj bachu <rajswaraj.r@gmail.com>
2022-09-18 15:50:35 +0530

    Fixed bugs and __init__ state error

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -10,10 +10,21 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 
+detector = htm.HandDetector()
+
 
 while True:
     success, img = cap.read()
     img = cv2.flip(img, 1)
+    detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        x1,x2 = lmList[4][1], lmList[8][1]
+        y1,y2 = lmList[4][2], lmList[8][2]
+
+        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
